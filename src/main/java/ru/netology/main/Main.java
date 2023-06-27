f78b33449f75d0bcdcd247af308fdbec48c54c07
Demitros <serrasalmus.80@gmail.com>
2023-06-27 10:51:30 +0500

    added

---
+++ src/main/java/ru/netology/main/Main.java
@@ -0,0 +1,20 @@
+package ru.netology.main;
+
+import ru.netology.oop.FormDate;
+import ru.netology.oop.Post;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.name = "Юрий";
+        post.patronymic = "Алексеевич";
+        post.surname = "Гагарин";
+        post.birthday.day = 9;
+        post.birthday.month = 3;
+        post.birthday.year = 1934;
+        post.passport = "1961 № 090334";
+        post.phone = "+7 (921)-124-19-61";
+        post.subscription = true;
+    }
+}
